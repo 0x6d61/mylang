ceb1ad50d1a8dd7f7213fee02cbe0b61cbb80f46
0x6d61 <0x6d61@gmail.com>
2021-06-17 00:57:04 +0900

    floatを計算可能に

--- src/lexer.mll
+++ src/lexer.mll
@@ -3,8 +3,8 @@ open Parser
 }
 
 
-let digit = ['0'-'9']
-let number = '-'? digit digit*
+let float = '-'? ((['1'-'9']['0'-'9']*)|'0')('.'['0'-'9']['0'-'9']*)
+let number = '-' ? ((['1'-'9']['0'-'9']*)|'0')
 let whitespace = ['\t' ' ' '\n' '\r']
 rule tokenize = parse
     | whitespace+ {tokenize lexbuf}
@@ -16,5 +16,6 @@ rule tokenize = parse
     | "/" {DIV}
     | "%" {MOD}
     | number as n {INT (int_of_string n)}
+    | float as n {FLOAT (float_of_string n)}
     | eof {EOL}
 
---
+++ src/util.ml
@@ -0,0 +1,68 @@
+open Char
+
+exception Error of string
+
+let err s = raise (Error s)
+
+
+let number value = match value with
+  | Ast.Int n -> n
+  |_ -> err("CastError: Not Integer")
+
+let bool value = match value with
+  | Ast.Bool n -> n
+  | _ -> err("CastError: Not Boolean")
+
+let string value = match value with
+  | Ast.String n -> n
+  | _ -> err("CastError: Not String")
+let char value = match value with
+  | Ast.Char n -> n
+  | _ -> err("CastError: Not Char")
+
+let float value = match value with
+  | Ast.Float n -> n
+  | _ -> err("CastError: Not Float")
+
+let to_string expr = match expr with
+  | Ast.Bool b -> b |> string_of_bool
+  | Ast.Int i -> i |> string_of_int
+  | Ast.String s -> s
+  | Ast.Float f -> f |> string_of_float
+  | Ast.Char c -> c |> escaped
+  | _ -> err("Not to Display")
+
+let add n m =
+  match n with
+  | Ast.Int _ -> Ast.Int (number n +  number m)
+  | Ast.Float _ -> Ast.Float (float n +. float  m)
+  | Ast.String _-> Ast.String (string n ^ string  m)
+  | _ -> err("Not Called Add Operater")
+let sub n m =
+  match n with 
+  | Ast.Int _ -> Ast.Int (number  n - number m)
+  | Ast.Float _ -> Ast.Float (float n  -. float  m)
+  | _ -> err("Not Colled Sub Operater")
+let mul n m = 
+  match n with 
+  | Ast.Int _ -> Ast.Int (number n * number m)
+  | Ast.Float _ ->Ast.Float (float n *. float m)
+  | _ -> err("Not Colled Mul Operater")
+let div n m = 
+  match n with
+  | Ast.Int _ -> let mm = number m in (
+      match mm with 
+      | 0 -> err("Division by zero")
+      | _ -> Ast.Int (number  n / mm))
+  | Ast.Float _ -> let mm = float m in (
+      match mm with 
+      | 0.0 -> err("Division by zero")
+      | _ -> Ast.Float (float  n /. mm))
+  | _ -> err("Not Colled Div Operater")
+let modd n m = match n with
+  | Ast.Int _ -> let mm = number m in (
+      match mm with 
+      | 0 -> err("Division by zero")
+      | _ -> Ast.Int (number  n mod mm))
+  | _ -> err("Not Colled Mod Operater")
+
